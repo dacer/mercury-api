03fc832155af91dd48ae6532cf589c42e538de88
Dacer <qqwerrd@gmail.com>
2019-11-05 15:23:07 +0900

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const Mercury = require('@postlight/mercury-parser')
+const agent = require('secure-random-user-agent')
+const express = require('express')
+const app = express()
+const port = 9697
+
+app.use(express.json());
+app.post('/parse', (req, res) => {
+    Mercury.parse(req.body.url, {
+        headers: {
+            'User-Agent': agent(),
+        },
+        contentType: 'markdown'
+    }).then(result => res.send(result));
+})
+
+app.listen(port, () => console.log(`mercury-api listening on port ${port}!`))
